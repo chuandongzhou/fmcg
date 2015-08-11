3c31442c06970147704ee320c8a960b16d6f1f31
Tinpont <510499887@qq.com>
2015-08-11 16:54:55 +0800

    更新后台样式

--- resources/views/admin/master.blade.php
+++ resources/views/admin/master.blade.php
@@ -17,109 +17,113 @@
     <div class="container-fluid admin-container">
         <div class="row">
             <div class="col-sm-2">
-                <div class="panel-group text-center" id="accordion">
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h4 class="panel-title">
-                                <a data-toggle="collapse" data-parent="#accordion"
-                                   href="#collapse-one">
-                                    系统管理
-                                </a>
-                            </h4>
-                        </div>
-                        <div id="collapse-one" class="panel-collapse collapse in">
-                            <div class="panel-body">
-                                <ul>
-                                    <li><a href="#">角色添加</a> <a href="#" class="manger">管理</a></li>
-                                    <li><a href="#">菜单添加</a> <a href="#" class="manger">管理</a></li>
-                                    <li><a href="{{url('admin/admin/create')}}">管理员添加</a> <a href="#"
-                                                                                             class="manger">管理</a></li>
-                                    <li><a href="#">密码修改</a></li>
+                <div class="left-container">
+                    <div class="panel-group text-center" id="accordion">
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a data-toggle="collapse" data-parent="#accordion"
+                                       href="#collapse-one">
+                                        系统管理
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapse-one" class="panel-collapse collapse in">
+                                <div class="panel-body">
+                                    <ul>
+                                        <li><a href="#">角色添加</a> <a href="#" class="manger">管理</a></li>
+                                        <li><a href="#">菜单添加</a> <a href="#" class="manger">管理</a></li>
+                                        <li>
+                                            <a href="{{url('admin/admin/create')}}">管理员添加</a>
+                                            <a href="#" class="manger">管理</a>
+                                        </li>
+                                        <li><a href="#">密码修改</a></li>
 
-                                </ul>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h4 class="panel-title">
-                                <a data-toggle="collapse" data-parent="#accordion"
-                                   href="#collapse-two">
-                                    账号管理
-                                </a>
-                            </h4>
-                        </div>
-                        <div id="collapse-two" class="panel-collapse collapse">
-                            <div class="panel-body">
-                                <ul>
-                                    <li><a href="{{ url('/admin/wholesalers/create')  }}">批发商添加</a> <a
-                                                href="{{ url('/admin/wholesalers')  }}" class="manger">管理</a></li>
-                                    <li><a href="#">终端商添加</a> <a href="#" class="manger">管理</a></li>
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a data-toggle="collapse" data-parent="#accordion"
+                                       href="#collapse-two">
+                                        账号管理
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapse-two" class="panel-collapse collapse">
+                                <div class="panel-body">
+                                    <ul>
+                                        <li><a href="{{ url('/admin/wholesalers/create')  }}">批发商添加</a> <a
+                                                    href="{{ url('/admin/wholesalers')  }}" class="manger">管理</a></li>
+                                        <li><a href="#">终端商添加</a> <a href="#" class="manger">管理</a></li>
 
-                                </ul>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h4 class="panel-title">
-                                <a data-toggle="collapse" data-parent="#accordion"
-                                   href="#collapse-three">
-                                    商品管理
-                                </a>
-                            </h4>
-                        </div>
-                        <div id="collapse-three" class="panel-collapse collapse">
-                            <div class="panel-body">
-                                <ul>
-                                    <li><a href="#">角色添加</a> <a href="#" class="manger">管理</a></li>
-                                    <li><a href="#">菜单添加</a> <a href="#" class="manger">管理</a></li>
-                                    <li><a href="#">管理员添加</a> <a href="#" class="manger">管理</a></li>
-                                    <li><a href="#">密码修改</a></li>
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a data-toggle="collapse" data-parent="#accordion"
+                                       href="#collapse-three">
+                                        商品管理
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapse-three" class="panel-collapse collapse">
+                                <div class="panel-body">
+                                    <ul>
+                                        <li><a href="#">角色添加</a> <a href="#" class="manger">管理</a></li>
+                                        <li><a href="#">菜单添加</a> <a href="#" class="manger">管理</a></li>
+                                        <li><a href="#">管理员添加</a> <a href="#" class="manger">管理</a></li>
+                                        <li><a href="#">密码修改</a></li>
 
-                                </ul>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h4 class="panel-title">
-                                <a data-toggle="collapse" data-parent="#accordion"
-                                   href="#collapse-four">
-                                    广告投放
-                                </a>
-                            </h4>
-                        </div>
-                        <div id="collapse-four" class="panel-collapse collapse">
-                            <div class="panel-body">
-                                <ul>
-                                    <li><a href="#">角色添加</a> <a href="#" class="manger">管理</a></li>
-                                    <li><a href="#">菜单添加</a> <a href="#" class="manger">管理</a></li>
-                                    <li><a href="#">管理员添加</a> <a href="#" class="manger">管理</a></li>
-                                    <li><a href="#">密码修改</a></li>
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a data-toggle="collapse" data-parent="#accordion"
+                                       href="#collapse-four">
+                                        广告投放
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapse-four" class="panel-collapse collapse">
+                                <div class="panel-body">
+                                    <ul>
+                                        <li><a href="#">角色添加</a> <a href="#" class="manger">管理</a></li>
+                                        <li><a href="#">菜单添加</a> <a href="#" class="manger">管理</a></li>
+                                        <li><a href="#">管理员添加</a> <a href="#" class="manger">管理</a></li>
+                                        <li><a href="#">密码修改</a></li>
 
-                                </ul>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="panel panel-default">
-                        <div class="panel-heading">
-                            <h4 class="panel-title">
-                                <a data-toggle="collapse" data-parent="#accordion"
-                                   href="#collapse-five">
-                                    平台交易
-                                </a>
-                            </h4>
-                        </div>
-                        <div id="collapse-five" class="panel-collapse collapse">
-                            <div class="panel-body">
-                                <ul>
-                                    <li><a href="#">角色添加</a> <a href="#" class="manger">管理</a></li>
-                                    <li><a href="#">菜单添加</a> <a href="#" class="manger">管理</a></li>
-                                    <li><a href="#">管理员添加</a> <a href="#" class="manger">管理</a></li>
-                                    <li><a href="#">密码修改</a></li>
+                        <div class="panel panel-default">
+                            <div class="panel-heading">
+                                <h4 class="panel-title">
+                                    <a data-toggle="collapse" data-parent="#accordion"
+                                       href="#collapse-five">
+                                        平台交易
+                                    </a>
+                                </h4>
+                            </div>
+                            <div id="collapse-five" class="panel-collapse collapse">
+                                <div class="panel-body">
+                                    <ul>
+                                        <li><a href="#">角色添加</a> <a href="#" class="manger">管理</a></li>
+                                        <li><a href="#">菜单添加</a> <a href="#" class="manger">管理</a></li>
+                                        <li><a href="#">管理员添加</a> <a href="#" class="manger">管理</a></li>
+                                        <li><a href="#">密码修改</a></li>
 
-                                </ul>
+                                    </ul>
+                                </div>
                             </div>
                         </div>
                     </div>
