acf93cf91eb635b2fefc27ebfb2fa29efa963d36
yinweiyi <378772944@qq.com>
2017-07-20 17:53:22 +0800

    优化是否app访问判断条件

--- app/Http/Middleware/RedirectForDifferentClient.php
+++ app/Http/Middleware/RedirectForDifferentClient.php
@@ -6,6 +6,11 @@ use Closure;
 
 class RedirectForDifferentClient
 {
+
+    protected $except = [
+        'api/v1/version',
+        'upload/file/*'
+    ];
     /**
      * Handle an incoming request.
      *
@@ -15,12 +20,12 @@ class RedirectForDifferentClient
      */
     public function handle($request, Closure $next)
     {
-        $terTypes = cons('ter_types');
+
+        $isApp = $request->is('api/v1/*') && !$request->ajax();
 
         $inWindows = in_windows();
-        if ($request->input('ter_type', head($terTypes)) === head($terTypes)) {
+        if (!$isApp) {
             $mobileUrl = 'http://m.dingbaida.com';
-
             if ($inWindows && false !== strpos($request->root(), $mobileUrl)) {
                     return redirect('http://dingbaida.com');
             } else if (!$inWindows && false === strpos($request->root(), $mobileUrl)) {
